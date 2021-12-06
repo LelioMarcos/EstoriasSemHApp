ef865e161bd77eb6ad7fc11957b3eeb405aa4e78
LelioMarcos <leliomrcunha@gmail.com>
2021-12-06 11:14:06 -0300

    layout 2

--- app/src/main/java/com/example/estoriassemhapp/MainActivity.java
+++ app/src/main/java/com/example/estoriassemhapp/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.estoriassemhapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //MainActivityViewModel vm = new ViewModelProvider(this).get(MainActivityViewModel.class);
+
+        // Ações para a seleção da guia inferior de escolha de fragments.
+        bottomNavigationView = findViewById(R.id.btNav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.homeViewOp: // Se a visualização por grid for selecionada, exibir em formato grid.
+                        MainFragment mainFragment = MainFragment.newInstance();
+                        setFragment(mainFragment);
+                        break;
+                    case R.id.profileViewOp: // Se a visualização por grid for selecionada, exibir em formato grid.
+                        ProfileFragment profileFragment = ProfileFragment.newInstance();
+                        setFragment(profileFragment);
+                        break;
+                    case R.id.writeViewOp: // Se a visualização por grid for selecionada, exibir em formato grid.
+                        WriteViewFragment writeViewFragment = WriteViewFragment.newInstance();
+                        setFragment(writeViewFragment);
+                        break;
+                    case R.id.tagsViewOp: // Se a visualização por grid for selecionada, exibir em formato grid.
+                        break;
+                    case R.id.exitOp: // Se a visualização por grid for selecionada, exibir em formato grid.
+                        break;
+                }
+
+                return true;
+            }
+        });
+    }
+
+    void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragContainer, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/estoriassemhapp/WriteViewFragment.java
@@ -0,0 +1,48 @@
+package com.example.estoriassemhapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ProfileFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class WriteViewFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public WriteViewFragment() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static WriteViewFragment newInstance() {
+        WriteViewFragment fragment = new WriteViewFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_write, container, false);
+    }
+}
