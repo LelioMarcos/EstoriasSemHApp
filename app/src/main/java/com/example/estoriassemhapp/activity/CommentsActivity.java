7170926ed8d004651c3d9f433ffb58edc410ad17
Carlos Eugênio Venturin Goldner <carloseugventurin@gmail.com>
2021-12-18 14:54:32 -0300

    comecando tela coment (ainda n ta funcionando)

--- app/src/main/java/com/example/estoriassemhapp/activity/CommentsActivity.java
+++ app/src/main/java/com/example/estoriassemhapp/activity/CommentsActivity.java
@@ -3,12 +3,23 @@ package com.example.estoriassemhapp.activity;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 
 import com.example.estoriassemhapp.R;
+import com.example.estoriassemhapp.adapter.CommentsAdapter;
+import com.example.estoriassemhapp.model.Comment;
+import com.example.estoriassemhapp.model.CommentsViewModel;
+
+import java.util.List;
 
 public class CommentsActivity extends AppCompatActivity {
 
@@ -20,9 +31,36 @@ public class CommentsActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.tbMain);
         setSupportActionBar(toolbar);
 
+        /*
+        Intent i =getIntent();
+        String id = i.getStringExtra("id");
+
+        CommentsViewModel commentsViewModel = new ViewModelProvider(this, new CommentsViewModel.CommentsViewModelFactory(id)).get(CommentsViewModel.class);
+
         // Adicionar a opção de voltar para a página inicial na toolbar.
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        //Configuração do recycler view
+        RecyclerView rvComments = findViewById(R.id.rvComments);
+        rvComments.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        rvComments.setLayoutManager(layoutManager);
+
+
+        //Configuração do Adapter (parte do recycler view)
+        LiveData<List<Comment>> comments = commentsViewModel.getComments();
+        //Atualização automática da lista de produtos
+        comments.observe(this, new Observer<List<Comment>>() {
+            @Override
+            public void onChanged(List<Comment> comments1) {
+                CommentsAdapter commentsAdapter = new CommentsAdapter(CommentsActivity.this, comments1);
+                rvComments.setAdapter(commentsAdapter);
+            }
+        });
+        */
+
+
     }
 
     @Override
---
+++ app/src/main/java/com/example/estoriassemhapp/model/CommentsViewModel.java
@@ -0,0 +1,106 @@
+package com.example.estoriassemhapp.model;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.estoriassemhapp.R;
+import com.example.estoriassemhapp.util.Config;
+import com.example.estoriassemhapp.util.HttpRequest;
+import com.example.estoriassemhapp.util.Util;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CommentsViewModel extends ViewModel {
+    String id;
+    MutableLiveData<List<Comment>> commentPost;
+    int navigationOpSelected = R.id.homeViewOp;
+
+    public CommentsViewModel(String id) {
+        this.id = id;
+    }
+
+    public LiveData<List<Comment>> getComments() {
+        if (commentPost==null) {
+            commentPost = new MutableLiveData<List<Comment>>();
+            loadComments();
+        }
+
+        return commentPost;
+    }
+
+    public void refreshComments() {
+        loadComments();
+    }
+
+    void loadComments() {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(new Runnable() {
+
+            @Override
+            public void run() {
+                List<Comment> commentsList = new ArrayList<>();
+                HttpRequest httpRequest = new HttpRequest(Config.BD_APP_URl + "comments/get_comments.php", "GET", "UTF-8");
+                httpRequest.addParam("id_story", id);
+
+                try {
+                    InputStream is = httpRequest.execute();
+                    String result = Util.inputStream2String(is, "UTF-8");
+                    httpRequest.finish();
+
+                    Log.d("HTTP_REQUEST_RESULT", result);
+
+                    JSONObject jsonObject = new JSONObject(result);
+                    int success = jsonObject.getInt("success");
+                    if (success == 1) {
+                        JSONArray jsonArray = jsonObject.getJSONArray("data");
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject jComments = jsonArray.getJSONObject(i);
+
+                            String idcoment = jComments.getString("idcoment");
+                            String idusu = jComments.getString("idusuario");
+                            String comentaro = jComments.getString("dsccorpocoment");
+
+                            Comment comment = new Comment(idcoment, idusu, comentaro);
+                            commentsList.add(comment);
+                        }
+                        commentPost.postValue(commentsList);
+                    }
+
+                }
+                catch (IOException | JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    static public class CommentsViewModelFactory implements ViewModelProvider.Factory {
+
+        String id;
+
+        public CommentsViewModelFactory(String id) {
+            this.id = id;
+        }
+
+        @NonNull
+        @Override
+        public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+            return (T) new StoryViewModel(id);
+        }
+    }
+
+}
