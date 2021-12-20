15e56ad65fb06ecf77dc0540f6bc7fb6bbec8d16
LelioMarcos <leliomrcunha@gmail.com>
2021-12-20 10:44:46 -0300

    search

--- app/src/main/java/com/example/estoriassemhapp/activity/SearchActivity.java
+++ app/src/main/java/com/example/estoriassemhapp/activity/SearchActivity.java
@@ -3,6 +3,11 @@ package com.example.estoriassemhapp.activity;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,6 +16,13 @@ import android.view.Menu;
 import android.view.MenuInflater;
 
 import com.example.estoriassemhapp.R;
+import com.example.estoriassemhapp.adapter.StoryAdapter;
+import com.example.estoriassemhapp.model.CommentsViewModel;
+import com.example.estoriassemhapp.model.MainViewModel;
+import com.example.estoriassemhapp.model.SearchViewModel;
+import com.example.estoriassemhapp.model.Story;
+
+import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -29,7 +41,22 @@ public class SearchActivity extends AppCompatActivity {
         Intent i = getIntent();
         String query = i.getStringExtra("query");
 
-        Log.i("search", query);
+
+        RecyclerView rvSearchResult = findViewById(R.id.rvSearchResult);
+        rvSearchResult.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        rvSearchResult.setLayoutManager(layoutManager);
+
+        //Configuração do Adapter (parte do recycler view)
+        SearchViewModel searchViewModel = new ViewModelProvider(SearchActivity.this, new SearchViewModel.SearchViewModelFactory(query)).get(SearchViewModel.class);
+        LiveData<List<Story>> stories = searchViewModel.getStories();
+        stories.observe(this, new Observer<List<Story>>() {
+            @Override
+            public void onChanged(List<Story> stories) {
+                StoryAdapter storyAdapter = new StoryAdapter(SearchActivity.this, stories);
+                rvSearchResult.setAdapter(storyAdapter);
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/example/estoriassemhapp/model/SearchViewModel.java
@@ -0,0 +1,102 @@
+package com.example.estoriassemhapp.model;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.estoriassemhapp.util.HttpRequest;
+import com.example.estoriassemhapp.util.Util;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SearchViewModel extends ViewModel {
+    String query;
+    MutableLiveData<List<Story>> stories;
+
+    public SearchViewModel(String query) {
+        this.query = query;
+    }
+
+    public LiveData<List<Story>> getStories() {
+        if (stories==null) {
+            stories = new MutableLiveData<List<Story>>();
+            loadStories();
+        }
+
+        return stories;
+    }
+
+    public void refreshStories() {
+        loadStories();
+    }
+
+    void loadStories() {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                List<Story> storiesList = new ArrayList<>();
+                HttpRequest httpRequest = new HttpRequest("https://estorias-sem-h-crud.herokuapp.com/stories/get_stories.php", "GET", "UTF-8");
+
+                httpRequest.addParam("search", query);
+
+                try {
+                    InputStream is = httpRequest.execute();
+                    String result = Util.inputStream2String(is, "UTF-8");
+                    httpRequest.finish();
+
+                    Log.d("HTTP_REQUEST_RESULT", result);
+
+                    JSONObject jsonObject = new JSONObject(result);
+                    int success = jsonObject.getInt("success");
+                    if (success == 1) {
+                        JSONArray jsonArray = jsonObject.getJSONArray("data");
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject jProduct = jsonArray.getJSONObject(i);
+
+                            String id = jProduct.getString("idhist");
+                            String title = jProduct.getString("nomhist");
+                            String text = jProduct.getString("dscsinopsehist");
+
+                            Story story = new Story(id, title, text);
+                            storiesList.add(story);
+                        }
+                        stories.postValue(storiesList);
+                    }
+
+                }
+                catch (IOException | JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    static public class SearchViewModelFactory implements ViewModelProvider.Factory {
+
+        String query;
+
+        public SearchViewModelFactory(String query) {
+            this.query = query;
+        }
+
+        @NonNull
+        @Override
+        public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+            return (T) new SearchViewModel(query);
+        }
+    }
+}
