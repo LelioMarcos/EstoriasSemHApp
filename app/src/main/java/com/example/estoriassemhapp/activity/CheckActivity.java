ed96e4e9ec9ed5be6cc2faac411f3fa337c35876
01737 <59713688+LelioMarcos@users.noreply.github.com>
2021-12-28 17:53:13 -0300

    op

--- app/src/main/java/com/example/estoriassemhapp/activity/CheckActivity.java
+++ app/src/main/java/com/example/estoriassemhapp/activity/CheckActivity.java
@@ -1,18 +1,35 @@
 package com.example.estoriassemhapp.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.example.estoriassemhapp.R;
 import com.example.estoriassemhapp.util.Config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CheckActivity extends AppCompatActivity {
+
+    static int RESULT_REQUEST_PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        List<String> permissions = new ArrayList<>();
+        permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        checkForPermissions(permissions);
 
         if(Config.getLogin(CheckActivity.this).isEmpty()) {
             Intent i = new Intent(CheckActivity.this, LoginActivity.class);
@@ -23,6 +40,59 @@ public class CheckActivity extends AppCompatActivity {
             Intent i = new Intent(CheckActivity.this, MainActivity.class);
             startActivity(i);
             finish();
+        }
+    }
+
+    private void checkForPermissions(List<String> permissions) {
+        List<String> permissionsNotGranted = new ArrayList<>();
+
+        for (String permission : permissions) {
+            if (!hasPermission(permission)) {
+                permissionsNotGranted.add(permission);
+            }
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (permissionsNotGranted.size() > 0) {
+                requestPermissions(permissionsNotGranted.toArray(new String[permissionsNotGranted.size()]), RESULT_REQUEST_PERMISSION);
+            }
+        }
+    }
+
+    //Verifica se uma determinada permissão já foi concedida
+    private boolean hasPermission(String permission) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return ActivityCompat.checkSelfPermission(CheckActivity.this, permission) == PackageManager.PERMISSION_GRANTED;
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        List<String> permissionRejected = new ArrayList<>();
+        if (requestCode == RESULT_REQUEST_PERMISSION) {
+            for (String permission : permissions) {
+                if (!hasPermission(permission)) {
+                    permissionRejected.add(permission);
+                }
+            }
+        }
+
+        if (permissionRejected.size() > 0) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (shouldShowRequestPermissionRationale(permissionRejected.get(0))) {
+                    new AlertDialog.Builder(CheckActivity.this).
+                            setMessage("Para usar esse app é preciso conceder essas permissões").
+                            setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    requestPermissions(permissionRejected.toArray(new String[permissionRejected.size()]), RESULT_REQUEST_PERMISSION);
+                                }
+                            }).create().show();
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/estoriassemhapp/activity/RegisterActivity.java
+++ app/src/main/java/com/example/estoriassemhapp/activity/RegisterActivity.java
@@ -1,14 +1,20 @@
 package com.example.estoriassemhapp.activity;
 
+import static com.google.protobuf.CodedOutputStream.DEFAULT_BUFFER_SIZE;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,14 +32,17 @@ import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class RegisterActivity extends AppCompatActivity {
 
     static int PHOTO_PICKER_REQUEST = 1;
+    static int DEFAULT_BUFFER_SIZE = 8192;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +61,7 @@ public class RegisterActivity extends AppCompatActivity {
         btnAddPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent photoPickerIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
                 photoPickerIntent.setType("image/*");
                 startActivityForResult(photoPickerIntent, PHOTO_PICKER_REQUEST);
             }
@@ -70,6 +79,15 @@ public class RegisterActivity extends AppCompatActivity {
                     Toast.makeText(RegisterActivity.this, "Você precisa selecionar uma imagem", Toast.LENGTH_LONG).show();
                     return;
                 }
+
+                InputStream in = null;
+                try {
+                    in = getContentResolver().openInputStream(selectPhotoLocation);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println(selectPhotoLocation);
 
                 EditText etNewUsername =  findViewById(R.id.etNewUsername);
                 final String newUsername = etNewUsername.getText().toString();
@@ -104,7 +122,6 @@ public class RegisterActivity extends AppCompatActivity {
                     return;
                 }
 
-
                 Intent i = new Intent();
                 i.setData(selectPhotoLocation);
                 setResult(Activity.RESULT_OK, i);
@@ -117,7 +134,7 @@ public class RegisterActivity extends AppCompatActivity {
                         httpRequest.addParam("newName", newUsername);
                         httpRequest.addParam("newLogin", newLogin);
                         httpRequest.addParam("newPassword", newPassword);
-                        httpRequest.addFile("newPhoto", new File (selectPhotoLocation.getPath()));
+                        httpRequest.addFile("newPhoto", new File(rvm.getSelectPhotoLocation().getPath()));
 
                         try {
                             InputStream is = httpRequest.execute();
@@ -153,6 +170,20 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
+    private static void copyInputStreamToFile(InputStream inputStream, File file)
+            throws IOException {
+
+        // append = false
+        try (FileOutputStream outputStream = new FileOutputStream(file, false)) {
+            int read;
+            byte[] bytes = new byte[DEFAULT_BUFFER_SIZE];
+            while ((read = inputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+        }
+
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -160,6 +191,8 @@ public class RegisterActivity extends AppCompatActivity {
         if(requestCode == PHOTO_PICKER_REQUEST) {
             if(resultCode == Activity.RESULT_OK) {
                 Uri selectPhotoLocation = data.getData(); //Pega o caminho da imagem
+
+                System.out.println(selectPhotoLocation.getPath());
                 //Insere a foto no preview
                 ImageView imvPhotoPreview = findViewById(R.id.imvPreview);
                 imvPhotoPreview.setImageURI(selectPhotoLocation);
