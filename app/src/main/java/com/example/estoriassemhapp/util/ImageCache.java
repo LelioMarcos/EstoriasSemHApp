4ebe00e1fed728758622f3fd2c22e75607d10653
Carlos Eugênio Venturin Goldner <carloseugventurin@gmail.com>
2022-01-03 18:37:57 -0300

    image cache

---
+++ app/src/main/java/com/example/estoriassemhapp/util/ImageCache.java
@@ -0,0 +1,58 @@
+package com.example.estoriassemhapp.util;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ImageCache {
+
+    public static void loadToImageView(Activity activity, String id, ImageView imageView) {
+        //Verificar se a imagem já está salva localmente
+        String imageLocation = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES) + "/" + id;
+        File f = new File(imageLocation);
+        if (f.exists() && !f.isDirectory()) {
+            imageView.setImageBitmap(Util.getBitmap(imageLocation));
+        }
+        else {
+            ExecutorService executorService = Executors.newSingleThreadExecutor();
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    /*
+                    HttpRequest httpRequest = new HttpRequest(Config.BD_APP_URl + "get_product_image.php", "GET", "UTF-8");
+                    httpRequest.addParam("id", id);
+                     */
+
+                    try {
+                        InputStream is = httpRequest.execute();
+                        String imgBase64 = Util.inputStream2String(is, "UTF-8");
+                        httpRequest.finish();
+
+                        String pureBase64Encoded = imgBase64.substring(imgBase64.indexOf(",") + 1);
+                        Bitmap img = Util.base642Bitmap(pureBase64Encoded);
+
+                        Util.saveImage(img, imageLocation);
+
+                        activity.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                imageView.setImageBitmap(img);
+                            }
+                        });
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+
+}
