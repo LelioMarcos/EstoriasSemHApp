ea9ef0c878cc701529ed25225a44a6a97feda80e
01737 <59713688+LelioMarcos@users.noreply.github.com>
2022-01-19 15:17:07 -0300

    Profile exclusevly

--- app/src/main/java/com/example/estoriassemhapp/activity/ProfileActivity.java
+++ app/src/main/java/com/example/estoriassemhapp/activity/ProfileActivity.java
@@ -1,8 +1,12 @@
 package com.example.estoriassemhapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.estoriassemhapp.R;
 
@@ -12,5 +16,22 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Toolbar toolbar = findViewById(R.id.tbMain);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        // Criar a toolbar da activity.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.top_nav_menu, menu);
+
+        MenuItem myActionMenuItem = menu.findItem(R.id.opSearch);
+        myActionMenuItem.setVisible(false);
+
+        return true;
     }
 }
